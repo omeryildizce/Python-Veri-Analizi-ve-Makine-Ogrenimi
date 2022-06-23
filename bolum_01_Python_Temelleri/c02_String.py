882b06a806524ffc38add281caa55df8fc15f47d
Omer Yildiz <omeryildizce@gmail.com>
2022-06-23 15:01:22 +0300

    Create c02_String.py

---
+++ bolum_01_Python_Temelleri/c02_String.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+mesaj = "Merhaba dünya"
+
+print(mesaj)
+print(mesaj[2])
+print(mesaj[-1])
+print(mesaj[2:5])
+
+
+yeniMesaj = mesaj[8:13]
+
+mesaj2 = mesaj[:7]
+
+print(len(mesaj))
+
+mesaj3 = mesaj[-5:-2] 
+
+
+# lower upper
+
+print(mesaj.upper())
+print(mesaj.lower())
+
+
+print(mesaj.replace("ü","u"))
+
+bilgi = "Ömer Yıldız 20 Ankara"
+print(bilgi.split())
+print(bilgi.split()[0])
+
