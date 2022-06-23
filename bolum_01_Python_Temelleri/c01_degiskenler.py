a522d3049b35e0f902be283f67ca8cba2d6c403a
Omer Yildiz <omeryildizce@gmail.com>
2022-06-23 15:01:19 +0300

    Create c01_degiskenler.py

---
+++ bolum_01_Python_Temelleri/c01_degiskenler.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+sayi = 10 # integer
+deger = "10" # string
+
+
+tc = "12345678911"
+isim = "Ömer"
+soyisim = "YILDIZ"
+telefonNumarasi = "500 500 5000"
+floatSayi = 10.0
+
+print(float(tc) + 10)
+print(type(floatSayi))
+print(type(sayi))
+print(type(deger))
