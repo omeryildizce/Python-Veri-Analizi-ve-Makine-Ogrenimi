d48b4c9c62290b65d45af6a20dd8d9b8773e8134
Omer Yildiz <omeryildizce@gmail.com>
2022-06-24 17:11:10 +0300

    Create matematikModule.py

---
+++ bolum_06_Class/matematikModule.py
@@ -0,0 +1,17 @@
+def topla(sayi1, sayi2):
+        print("Toplam : " + str(sayi1 + sayi2))
+
+def cikar(sayi1, sayi2):
+        print("Fark : " + str(sayi1 - sayi2))
+
+def carp(sayi1, sayi2):
+        print("Çarpım : " + str(sayi1 * sayi2))
+
+def bol(sayi1, sayi2):
+        print("Bölüm : " + str(sayi1 / sayi2))
+
+musteri = {
+    "firstname" : "Ömer",
+    "lastname"  : "Yıldız",
+    "age"       : 30
+}
